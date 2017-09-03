4dfc5d793ec6ebcf34340ce27db63cadc66b2203
jy02sung <jy02sung@naver.com>
2017-09-03 20:07:40 +0900

    역이름 표시

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $host.mag();
 //To get api about subway location
 var stationName = " ";
 var APP_ID = "4f66435472776c74383857486b6d48";
-var authPreFix = "/json/realtimeStationArrival/1/3/";
+var authPreFix = "/json/realtimeStationArrival/1/4/";
 var currentStationURL = "https://cors-anywhere.herokuapp.com/http://swopenAPI.seoul.go.kr/api/subway/";
 var LocationData = {};
 
@@ -25,20 +25,43 @@ function requestSubwayInfo() {
         type: 'GET',
         url: requestURL,
         success: function(data) {
-            console.log(data);
             saveData(data);
         }
     });
 }
 
 function saveData(data) {
-    LocationData = JSON.stringify(data);
+    LocationData = data;
     console.log(LocationData);
     renderData();
 }
 
+function parseNum(data) {
+    if(data.split(" ")[0] === "전역") {
+        return 1;
+    } else if (data.split(" ")[1] === "진입" || data.split(" ")[1] === "도착") {
+        return 0;
+    } else {
+        return parseInt(data.split("[")[1]);
+    }
+}
+
 function renderData() {
-    // $("#subway-name .value").text(LocationData.main.temp);
+    $("#subway-way1").text(LocationData.realtimeArrivalList[0].trainLineNm);
+    $("#subway-way2").text(LocationData.realtimeArrivalList[1].trainLineNm);
+    $("#station1-1 .subway-name").text(LocationData.realtimeArrivalList[0].statnNm);
+    $("#station2-3 .subway-name").text(LocationData.realtimeArrivalList[0].statnNm);
+    $("#station1-2 .subway-name").text(LocationData.realtimeArrivalList[0].arvlMsg3);
+    $("#station2-1 .subway-name").text(LocationData.realtimeArrivalList[3].arvlMsg3);
+    $("#station1-3 .subway-name").text(LocationData.realtimeArrivalList[2].arvlMsg3);
+    $("#station2-2 .subway-name").text(LocationData.realtimeArrivalList[1].arvlMsg3);
+    $("#station1-2 .subway-dot").text(parseNum(LocationData.realtimeArrivalList[0].arvlMsg2));
+    $("#station1-3 .subway-dot").text(parseNum(LocationData.realtimeArrivalList[2].arvlMsg2));
+    $("#station2-1 .subway-dot").text(parseNum(LocationData.realtimeArrivalList[3].arvlMsg2));
+    $("#station2-2 .subway-dot").text(parseNum(LocationData.realtimeArrivalList[1].arvlMsg2));
+    
 }
 
 $("#search-btn").click(startSearch);
+
+
